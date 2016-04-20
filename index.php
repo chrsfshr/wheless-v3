39953d68321824343a021c2ce6d554fcaba003c0
CHRIS FISHER <clfisher@us.ibm.com>
2016-04-20 17:46:43 -0500

    adding wordpress theme files

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+<section class="content">
+	<div class="card">
+		<div class="card-img">
+			<img src="http://placehold.it/400x189">
+		</div>
+		<div class="card-content">
+			<h2>Powder apple pie chocolate</h2>
+			<p>Chocolate bar biscuit muffin toffee pastry. Chocolate bar jujubes sugar plum carrot cake chupa chups toffee chupa chups croissant.</p>
+		</div>
+		<div class="tags">
+			<p>#sermon</p> 
+			<p>#fellowship</p> 
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-img">
+			<img src="http://placehold.it/400x189">
+		</div>
+		<div class="card-content">
+			<h2>Powder apple pie chocolate</h2>
+			<p>Chocolate bar biscuit muffin toffee pastry. Chocolate bar jujubes sugar plum carrot cake chupa chups toffee chupa chups croissant.</p>
+		</div>
+		<div class="tags">
+			<p>#sermon</p> 
+			<p>#event</p> 
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-img">
+			<img src="http://placehold.it/400x189">
+		</div>
+		<div class="card-content">
+			<h2>Powder apple pie chocolate</h2>
+			<p>Chocolate bar biscuit muffin toffee pastry. Chocolate bar jujubes sugar plum carrot cake chupa chups toffee chupa chups croissant.</p>
+		</div>
+		<div class="tags">
+			<p>#sermon</p> 
+			<p>#love</p>  
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
